3bc655af3ec12bb3e577cb880b2d48aff0545678
Yeasin Tanim <YeasinTanim>
2018-10-18 20:11:04 +0600

    first commit

---
+++ HelloWorldWebApp/IndexUI.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWorldWebApp
+{
+    public partial class IndexUI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void showButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Value;
+            nameTextBox.Value = String.Empty;
+            outputLabel.Text = name;
+        }       
+    }
+}
